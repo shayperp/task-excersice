90d4cc531036431a723cb3299bd6c3ff8f66bcba
shayperp <shay.perp@gmail.com>
2022-05-22 09:49:07 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+def hello():
+    print('hi')
+
+
+def function():
+    con = []
+    for i in range(0, 10):
+        con.append(i)
+
+    for i in range(0, len(con)):
+        print(i)
+
+
+if __name__ == '__main__':
+    hello()
+    function()
